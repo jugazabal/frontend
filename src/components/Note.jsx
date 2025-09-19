eda92801d9ba5e0237c27473b561aa707a989ee2
jugazabal <jugazabal@gmail.com>
2025-09-19 15:23:11 +0300

    changed button color to blue

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,18 @@ const Note = ({ note, onToggle, onDelete }) => {
   return (
     <li>
       {note.content} 
-      <button onClick={onToggle}>
+      <button
+        onClick={onToggle}
+        style={{
+          color: note.important ? 'red' : 'blue',
+          borderColor: note.important ? 'red' : 'blue',
+          borderWidth: '1px',
+          borderStyle: 'solid',
+          background: 'white',
+          marginRight: '0.5em',
+          cursor: 'pointer'
+        }}
+      >
         make {note.important ? 'not important' : 'important'}
       </button>
       <button onClick={onDelete} style={{ marginLeft: '0.5em', color: 'red' }}>
